0ae8e8239549727ea9f4426a6e7bb2b75ac1cddc
vladponcea <vladponcea@icloud.com>
2023-02-02 14:26:51 +0200

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Vladut Mihai Poncea on 02.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
